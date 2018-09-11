087f2a4539a1d7fd5cde76ffb130554683c09fe3
nirvidya8147322767 <43023336+nirvidya8147322767@users.noreply.github.com>
2018-09-11 12:50:34 +0530

    Add files via upload

--- 11python.py
+++ 11python.py
@@ -1,10 +1,3 @@
-print("welcome to python lab")
-a=input("enter value for a: ")
-b=input("enter value for b: ")
-#add a and b
-if(a>b):
-    print(a,"is greater than",b)
-elif(a==b):
-     print(a,"is equal to", b)
-elif(b>a):
-	print(b,"is greater than",a)
+
+a=input("enter name a:")
+print("hello",a,"how are you")
