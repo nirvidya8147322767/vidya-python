8283061aba6fc0fd2e517bf30b41a5e548317a7e
nirvidya8147322767 <43023336+nirvidya8147322767@users.noreply.github.com>
2018-10-30 12:52:52 +0530

    Add files via upload

---
+++ graph2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplotlib as plt
+left=[1,2,3,4,5]
+height=[10,24,36,44,50]
+tick table=['one','two','three','four','five']
+plt.bar(left,height, tick_label = tick label,width =0.8,color =['blue','red'] )
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('bar graph')
+plt.show()
